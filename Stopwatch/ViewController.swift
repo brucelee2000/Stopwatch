2e319cfa3ab2f5aa634bb364300762e816846146
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-10 22:27:17 -0800

    Add usage for timer: Start/Set, stop/remove.

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var count = 0
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var recordLabel: UILabel!
+    
+    @IBAction func playButtonPressed(sender: UIBarButtonItem) {
+        if !timer.valid {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "myAction", userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func pauseButtonPressed(sender: UIBarButtonItem) {
+        // Stop the timer
+        timer.invalidate()
+    }
+    
+    @IBAction func resetButtonPressed(sender: UIBarButtonItem) {
+        // Stop the timer
+        timer.invalidate()
+        count = 0
+        timeLabel.text = "0"
+        recordLabel.hidden = true
+    }
+    
+    @IBAction func saveButtonPressed(sender: UIBarButtonItem) {
+        recordLabel.hidden = false
+        recordLabel.text = "Last record is at \(count)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +49,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func myAction() {
+        ++count
+        timeLabel.text = "\(count)"
     }
 
 
